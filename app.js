60f540e7dee9b5c957618a6800eb91e1aac62b45
willjoliver <124806724+willjoliver@users.noreply.github.com>
2025-05-14 16:26:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -302,7 +302,7 @@ function updateStreams() {
           }
           streamUrl = streamId;
         } else {
-          streamUrl = channel?.customUrl || `https://daddylive.mp/embed/stream-${streamId}.php`;
+          streamUrl = channel?.customUrl || `https://daddylive.dad/embed/stream-${streamId}.php`;
         }
 
         wrapper.innerHTML = `<iframe src="${streamUrl}" allowfullscreen></iframe>`;
