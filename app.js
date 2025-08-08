3720b9af0c44e9a090644809644f04e59bff4ff4
willjoliver <124806724+willjoliver@users.noreply.github.com>
2025-08-07 20:01:15 -0400

    Update app.js

--- app.js
+++ app.js
@@ -302,7 +302,7 @@ function updateStreams() {
           }
           streamUrl = streamId;
         } else {
-          streamUrl = channel?.customUrl || `https://dlhd.click/embed/stream-${streamId}.php`;
+          streamUrl = channel?.customUrl || `https://thedaddy.dad/embed/stream-${streamId}.php`;
         }
 
         wrapper.innerHTML = `<iframe src="${streamUrl}" allowfullscreen></iframe>`;
